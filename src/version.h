e2078c212f58165635c01eb28385615f6cbb3220
Jonathan <64264061+Jonathan-fattouh@users.noreply.github.com>
2023-04-17 18:03:15 +0200

    Update version.h

--- src/version.h
+++ src/version.h
@@ -19,7 +19,7 @@
 #ifndef __VERSION_H__
 #define __VERSION_H__
 
-#define FW_VERSION_STR "3.1.12.0"
-#define FW_VERSION 0x03010C00 // 3.1.12.0
+#define FW_VERSION_STR "3.1.13.0"
+#define FW_VERSION 0x03010D00 // 3.1.13.0
 
 #endif
